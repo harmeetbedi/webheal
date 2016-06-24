ab6b0d3cd0df78a66c49739c4d01eef8e86aa134
Harmeet <harmeet@kodemuse.com>
2016-06-24 11:47:54 -0400

    scanner

--- scanner/src/org/webheal/scanner/AppScanConfig.java
+++ scanner/src/org/webheal/scanner/AppScanConfig.java
@@ -22,6 +22,7 @@ public class AppScanConfig
     public final AttackAndResponseMatch rfi;
     public final List<String> attackBig;
     public final List<String> blindAttack;
+    public final List<String> dirListing;
     public final String proxyHost;
     public final int proxyPort;
     public final boolean proxyEnabled;
@@ -51,6 +52,7 @@ public class AppScanConfig
         lfiLinux = new LFIAttackAndResponseMatch(appScanDir,false);
         attackBig = Utils.readLines(new File(appScanDir,"sqlbig.txt"));
         blindAttack = Utils.readLines(new File(appScanDir,"blindsql.txt"));
+        dirListing = Utils.readLines(new File(appScanDir,"directory_listing_match.txt"));
     }
     
     private static AppScanConfig s_inst;
